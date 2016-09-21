e71e4d22e7367824cbc34307dce4a57ad622b2b2
hardcover <github@hardcoverwebdesign.com>
2016-09-21 13:19:32 -0700

    Add files via upload

--- sms.php
+++ sms.php
@@ -2,14 +2,14 @@
 /**
  * SMS Text Messager for sending SMS messages from the command line
  *
- * PHP version 5
+ * PHP version 7
  *
  * @category  SMS
  * @package   SMS-Text-Messager
  * @author    Hardcover LLC <useTheContactForm@hardcoverwebdesign.com>
- * @copyright 2013-2015 Hardcover LLC
+ * @copyright 2016 Hardcover LLC
  * @license   http://hardcoverwebdesign.com/license  MIT License
- * @version   GIT: 2015 05 08
+ * @version:  2016 01 06
  * @link      http://hardcoverwebdesign.com/
  * @link      https://github.com/hardcover/
  *
@@ -25,14 +25,13 @@
  * Execute the following command line
  * php sms.php from to "message"
  */
-
 if (isset($argv['1']) and isset($argv['2']) and isset($argv['3'])) {
     $to = $argv['2'] . "\r\n";
     $subject = "\r\n";
     $body = $argv['3'] . "\r\n";
-    $headers = 'From: ' . $argv['1'] . "\n";
-    $headers.= 'MIME-Version: 1.0' . "\n";
-    $headers.= 'Content-Type: text/plain; charset=utf-8; format=flowed' . "\n";
+    $headers = 'From: ' . $argv['1'] . "\r\n";
+    $headers.= 'MIME-Version: 1.0' . "\r\n";
+    $headers.= 'Content-Type: text/plain; charset=utf-8; format=flowed' . "\r\n";
     $headers.= 'Content-Transfer-Encoding: 7bit' . "\r\n";
     mail($to, $subject, $body, $headers);
 }
